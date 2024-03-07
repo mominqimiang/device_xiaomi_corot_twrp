7559a323297a98ee1eedd15203adb46ac4f7643f
莫名其妙2333 <50542889+mominqimiang@users.noreply.github.com>
2024-03-07 17:29:42 +0800

    Update init_corot.cpp

--- init/init_corot.cpp
+++ init/init_corot.cpp
@@ -24,13 +24,13 @@ void vendor_load_properties()
     string prop_partitions[] = {"", "vendor.", "odm."};
     for (const string &prop : prop_partitions)
     {
-        property_override(string("ro.product.") + prop + string("brand"), "Xiaomi");
+        property_override(string("ro.product.") + prop + string("brand"), "Redmi");
         property_override(string("ro.product.") + prop + string("manufacturer"), "Xiaomi");
-        property_override(string("ro.product.") + prop + string("name"), "aristotle");
-        property_override(string("ro.product.") + prop + string("device"), "aristotle");
-        property_override(string("ro.product.") + prop + string("model"), "2306EPN60G");
-        property_override(string("ro.product.") + prop + string("marketname"), "Xiaomi 13T");
-        property_override(string("ro.product.") + prop + string("cert"), "2306EPN60G");
+        property_override(string("ro.product.") + prop + string("name"), "corot");
+        property_override(string("ro.product.") + prop + string("device"), "corot");
+        property_override(string("ro.product.") + prop + string("model"), "23078RKD5C");
+        property_override(string("ro.product.") + prop + string("marketname"), "K60U");
+        property_override(string("ro.product.") + prop + string("cert"), "23078RKD5C");
     }
     property_override("ro.bootimage.build.date.utc", "1676018595");
     property_override("ro.build.date.utc", "1676018595");
