4a9837a98cd73d4ec23514b4bbef7084464a3a82
莫名其妙2333 <50542889+mominqimiang@users.noreply.github.com>
2024-03-10 17:23:42 +0800

    Update twrp_corot.mk

--- twrp_corot.mk
+++ twrp_corot.mk
@@ -1,19 +1,36 @@
-# Inherit from common AOSP config
+# Configure base.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Configure core_64_bit_only.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+
+# Configure launch_with_vendor_ramdisk.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
-# Inherit from TWRP product configuration
+# Configure emulated_storage.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Configure twrp config common.mk
 $(call inherit-product, vendor/twrp/config/common.mk)
+
+# Configure compression
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 
 # Device specific configs
 $(call inherit-product, device/xiaomi/corot/device.mk)
 
+# Configure virtual_ab_ota.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
 # Device identifier
 PRODUCT_DEVICE := corot
 PRODUCT_NAME := twrp_corot
-PRODUCT_BRAND := Redmi
+PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := 23078RKD5C
 PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_RELEASE_NAME := Redmi K60Ultra
 
-PRODUCT_PROPERTY_OVERRIDES += ro.twrp.vendor_boot=true
+# Hide Reflash TWRP & FUSE passthrough
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.twrp.vendor_boot=true \
+    persist.sys.fuse.passthrough.enable=true
